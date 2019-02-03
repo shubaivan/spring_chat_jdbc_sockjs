cacc658a9a7454085cb997a0cbee527f9190013d
shuba.ivan <i.shuba@kf.ua>
2019-02-03 16:11:44 +0200

    small changes

--- src/main/java/com/spdu/web/viewcontrollers/ChatViewController.java
+++ src/main/java/com/spdu/web/viewcontrollers/ChatViewController.java
@@ -2,6 +2,7 @@ package com.spdu.web.viewcontrollers;
 
 import com.spdu.bll.interfaces.ChatService;
 import com.spdu.bll.interfaces.UserService;
+import com.spdu.dal.repository.UserRepository;
 import com.spdu.domain_models.entities.Chat;
 import com.spdu.domain_models.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,17 +21,23 @@ import java.util.Optional;
 public class ChatViewController {
     private final ChatService chatService;
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public ChatViewController(ChatService chatService, UserService userService) {
+    public ChatViewController(
+            ChatService chatService,
+            UserService userService,
+            UserRepository userRepository
+    ) {
         this.chatService = chatService;
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
-    public String setChatsContent(ModelMap modelMap) {
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        Optional<User> user = userService.getByEmail(email);
+    public String setChatsContent(ModelMap modelMap, Principal principal) {
+//        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<User> user = userRepository.getByUserName(principal.getName());
         if (user.isPresent()) {
             List<Chat> ownChats = chatService.getAllOwn(user.get().getId());
             List<Chat> allChats = chatService.getAll(user.get().getId());
--- src/main/resources/static/js/mainform.js
+++ src/main/resources/static/js/mainform.js
@@ -1,3 +1,10 @@
+jQuery(document).ready(function () {
+    jQuery('.chats_info').on('click', function () {
+        var current = jQuery(this);
+        alert(current.attr('el_id'));
+    });
+});
+
 function openSideMenu() {
     document.getElementById('side-menu').style.width = '225px';
     document.getElementById('main').style.marginLeft = '225px';
