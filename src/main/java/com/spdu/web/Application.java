4487acca6f3856b25566b2b3738deec3f2146af9
Dmitry Ivashchenko <ivashcneko.dima@gmail.com>
2019-02-15 20:41:59 +0200

    Fixed

--- src/main/java/com/spdu/web/Application.java
+++ src/main/java/com/spdu/web/Application.java
@@ -2,6 +2,7 @@ package com.spdu.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.spdu")
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter("spd_talks.pid"));
+        springApplication.run(args);
     }
 }
