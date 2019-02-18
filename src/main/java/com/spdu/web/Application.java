b3de511d89e97049ec35b4933e5437458bab8746
Inna Bakum <ibakum95@gmail.com>
2019-02-18 12:40:52 +0200

    Changed deploy sett.

--- src/main/java/com/spdu/web/Application.java
+++ src/main/java/com/spdu/web/Application.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.spdu")
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        //SpringApplication.run(Application.class, args);
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.addListeners(new ApplicationPidFileWriter("spd_talks.pid"));
         springApplication.run(args);
