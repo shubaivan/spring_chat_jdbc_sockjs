7482c496ade47c073e323b3be4bd3d4c440a152f
Inna Bakum <ibakum95@gmail.com>
2019-02-20 15:48:26 +0200

    Changed application runner.

--- src/main/java/com/spdu/web/Application.java
+++ src/main/java/com/spdu/web/Application.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.spdu")
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter("spd_talks.pid"));
+        springApplication.run(args);
     }
 }
